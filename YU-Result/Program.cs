f1c32ed7b4e6f13da0b1540745c526fb3b8ad5b1
pd <uy30704991@163.com>
2020-01-07 18:28:02 +0800

    2020 1-7 16:27

--- YU-Result/Program.cs
+++ YU-Result/Program.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Y_DAL;
+using Y_Utils;
 
 namespace YU_Result
 {
@@ -16,28 +17,42 @@ namespace YU_Result
             //Console.WriteLine(Y_Utils.U_IP.GetInstance().GetRegionAsync("61.244.148.166").Result);
             //Console.WriteLine(Y_Utils.U_IP.GetInstance().GetRegionAsync("192.168.1.140").Result);
             //Console.WriteLine(Y_Utils.U_Time.TransitionToDateTime("1574748101736"));
+
             Console.WriteLine("*****************************");
-            DbContext context = new Y_DAL.ApplicationDbContext();
-            using (BIService.IUserInfoService userInfoService = new BService.UserInfoService(context))
-            {
-                try
-                {
-                    Y_DAL.AccountInfo accountInfo = userInfoService.Find<AccountInfo>(6622);
-                    string name = accountInfo.Name;
-                    AccountInfo accountInfo1 = userInfoService.GetAccountInfoByAccountId(6621);
-                    string phone = accountInfo1.Phone.ToString();
-                    var entities = userInfoService.Set<AccountInfo>();
-                    foreach (var item in entities)
-                    {
-                        Console.WriteLine(item.Name);
-                    }
-                }
-                catch (Exception err)
-                {
-                    Console.WriteLine(err.StackTrace);
-                }
-               
-            }
+
+            #region AES 加密解密测试
+            string key = "12345678876543211234567887654abc";
+            string val = "我是ALEX";
+            Console.WriteLine(val);
+            string encryptAes = U_Encrypt.AesEncrypt(val,key);
+            Console.WriteLine(encryptAes);
+            string decryptAes = U_Encrypt.AesDecrypt(encryptAes, key);
+            Console.WriteLine(decryptAes);
+            #endregion
+
+            #region 数据库访问层测试
+            //DbContext context = new Y_DAL.ApplicationDbContext();
+            //using (BIService.IUserInfoService userInfoService = new BService.UserInfoService(context))
+            //{
+            //    try
+            //    {
+            //        Y_DAL.AccountInfo accountInfo = userInfoService.Find<AccountInfo>(6622);
+            //        string name = accountInfo.Name;
+            //        AccountInfo accountInfo1 = userInfoService.GetAccountInfoByAccountId(6621);
+            //        string phone = accountInfo1.Phone.ToString();
+            //        var entities = userInfoService.Set<AccountInfo>();
+            //        foreach (var item in entities)
+            //        {
+            //            Console.WriteLine(item.Name);
+            //        }
+            //    }
+            //    catch (Exception err)
+            //    {
+            //        Console.WriteLine(err.StackTrace);
+            //    }
+            //}
+            #endregion
+
             Console.ReadLine();
         }
     }
---
+++ Y_Utils/U_Encrypt.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Y_Utils
+{
+    public class U_Encrypt
+    {
+        /// <summary>
+        /// AES 加密
+        /// </summary>
+        /// <param name="str">明文（待加密）</param>
+        /// <param name="key">密文</param>
+        /// <returns></returns>
+        public static string AesEncrypt(string str, string key, string vi="")
+        {
+            if (string.IsNullOrEmpty(str)) return null;
+            byte[] toEncryptArray = Encoding.UTF8.GetBytes(str);
+            //16位密钥=128位，24位密钥=192位，32位密钥=256位
+            RijndaelManaged rm = new RijndaelManaged
+            {
+                Key = Encoding.UTF8.GetBytes(key),
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7 
+            };
+
+            ICryptoTransform cTransform = rm.CreateEncryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+        /// <summary>
+        /// AES 解密
+        /// </summary>
+        /// <param name="str">明文（待解密）</param>
+        /// <param name="key">密文</param>
+        /// <returns></returns>
+        public static string AesDecrypt(string str, string key)
+        {
+            if (string.IsNullOrEmpty(str)) return null;
+            byte[] toEncryptArray = Convert.FromBase64String(str);
+
+            RijndaelManaged rm = new RijndaelManaged
+            {
+                Key = Encoding.UTF8.GetBytes(key),
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+
+            ICryptoTransform cTransform = rm.CreateDecryptor();
+            byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+            return Encoding.UTF8.GetString(resultArray);
+        }
+    }
+}
